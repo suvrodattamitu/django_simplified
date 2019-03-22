dfe879ccb55975ef7ed25c34978e592e33f0ee16
suvrodattamitu <suvrodatta95.com>
2019-03-22 14:26:17 +0600

    fetch databasa data

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view,contact_view,about_view,blog_view
+from products.views import product_detail_view
 
 urlpatterns = [
 	path('',home_view, name='home'),
 	path('contact/',contact_view),
 	path('about/',about_view),
-	path('blog/',blog_view),
+    path('blog/',blog_view),
+	path('product/',product_detail_view),
     path('admin/', admin.site.urls),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Product
+
+def product_detail_view(request):
+	obj = Product.objects.get(id=1)
+	context = {
+		'object':obj,
+	}
+	#return HttpResponse("suvro")
+	return render(request,"products/product_detail.html",context)
