d69e7e8e720b36049f3a5bafcb823986fb8026d3
suvrodattamitu <suvrodatta95.com>
2019-03-23 09:21:58 +0600

    raw form

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view,contact_view,about_view,blog_view
-from products.views import product_detail_view
+from products.views import product_detail_view,product_create_view
 
 urlpatterns = [
 	path('',home_view, name='home'),
 	path('contact/',contact_view),
 	path('about/',about_view),
     path('blog/',blog_view),
-	path('product/',product_detail_view),
+    path('product/',product_detail_view),
+	path('create/',product_create_view),
     path('admin/', admin.site.urls),
 ]
--- products/views.py
+++ products/views.py
@@ -3,6 +3,30 @@ from django.http import HttpResponse
 
 from .models import Product
 
+
+#raw html form
+def product_create_view(request):
+	if request.method == 'POST':
+
+
+		my_title = request.POST.get('title')
+		my_description = request.POST.get('description')
+		my_summary = request.POST.get('summary')
+		my_featured = request.POST.get('featured')
+		if request.POST.get('featured')  == None:
+			my_featured = 0
+		my_price = request.POST.get('price')
+		print(my_title)
+		print(my_description)
+		print(my_summary)
+		print(my_featured)
+		print(my_price)
+		#save data
+		Product.objects.create(title=my_title,description=my_description,price=my_price,summary=my_summary,featured=my_featured)
+
+	context = {}
+	return render(request,"products/product_create.html",context)
+
 def product_detail_view(request):
 	obj = Product.objects.get(id=1)
 	context = {
